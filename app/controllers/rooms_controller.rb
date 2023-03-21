2bdad7300d722df69b13e13cd80fd80830da7df7
mocchio <skmt.rome@gmail.com>
2023-03-21 16:30:03 +0900

    ルーム詳細、編集、削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @rooms = Room.includes(:users, :creator).order("created_at DESC")
   end
@@ -8,7 +10,7 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.new(room_params)
+    @room = Room.new(room_params.merge(creator: current_user))
     if @room.save
       redirect_to root_path
     else
@@ -16,8 +18,31 @@ class RoomsController < ApplicationController
     end
   end
 
+  def show
+    @room = Room.find(params[:id])
+  end
+
+  def edit
+    @room = Room.find(params[:id])
+  end
+
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      redirect_to room_path(@room)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy()
+    redirect_to root_path
+  end
+
   private
   def room_params
-    params.require(:room).permit(:name, :level_id, :habit, :rule, user_ids: []).merge(creator: current_user)
+    params.require(:room).permit(:name, :level_id, :habit, :rule, user_ids: [])
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,7 +5,7 @@ class Room < ApplicationRecord
   validates :name, :habit, :rule, presence: true
   validates :level_id, numericality: { other_than: 1, message: "can't be blank"} 
 
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
 
   belongs_to :creator, class_name: "User"
