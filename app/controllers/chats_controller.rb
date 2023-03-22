291350fa34b9f950c50639b73dda19c4ba7d8f96
mocchio <skmt.rome@gmail.com>
2023-03-22 14:58:20 +0900

    チャット送信機能の実装

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,21 @@
+class ChatsController < ApplicationController
+  def index
+    @chat = Chat.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @chat = @room.chats.new(chat_params)
+    if @chat.save
+      redirect_to room_chats_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:message).merge(user_id: current_user.id)
+  end
+end
