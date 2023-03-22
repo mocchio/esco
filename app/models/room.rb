fcf88065fd819f455056919b405708f10acb9966
mocchio <skmt.rome@gmail.com>
2023-03-22 17:49:55 +0900

    ルーム削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,7 +7,7 @@ class Room < ApplicationRecord
 
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :chats
+  has_many :chats, dependent: :destroy
 
   belongs_to :creator, class_name: "User"
 end
