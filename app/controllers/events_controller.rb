dfb83fcbc788431117ae5da28d7a34a7dea7a9ca
mocchio <skmt.rome@gmail.com>
2023-04-02 20:48:53 +0900

    カレンダー一覧の実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,26 @@
 class EventsController < ApplicationController
+  before_action :set_beginning_of_week
+
   def index
+    @events = Event.all
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_parameter)
+    if @event.save
+      redirect_to events_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def event_parameter
+    params.require(:event).permit(:title, :content, :start_time)
+  end
+
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
   end
 end
