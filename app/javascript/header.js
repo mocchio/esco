8aaba37d9c6d3f77664c72f022611d7bcb40ebcc
mocchio <skmt.rome@gmail.com>
2023-04-04 16:51:20 +0900

    ヘッダーjsファイル編集

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,83 +1,27 @@
 window.addEventListener('load', function(){
-  const headerMenu1 = document.getElementById("header-menu1");
-  const headerMenu2 = document.getElementById("header-menu2");
-  const headerMenu3 = document.getElementById("header-menu3");
-  const headerMenu4 = document.getElementById("header-menu4");
-  const pullDownMenu = document.getElementById("menu3-pull-down");
-  const userName = document.getElementById("user-name");
-  const logOut = document.getElementById("log-out-btn");
-  const logIn = document.getElementById("log-in-btn");
-  const userRegistration = document.getElementById("user-registration");
+  const headerMenus = document.querySelectorAll('#header-menu1, #header-menu2, #header-menu3, #header-menu4');
+  const pullDownMenu = document.getElementById('menu3-pull-down');
+  const links = document.querySelectorAll('#user-name, #log-out-btn, #log-in-btn, #user-registration');
 
-  headerMenu1.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  headerMenu1.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  headerMenu2.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  headerMenu2.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  headerMenu4.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  headerMenu4.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  headerMenu3.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  headerMenu3.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
+  for (let i = 0; i < headerMenus.length; i++) {
+    headerMenus[i].addEventListener('mouseover', function(){
+      this.style.textDecoration = 'underline';
+    });
+    headerMenus[i].addEventListener('mouseout', function(){
+      this.style.textDecoration = 'none';
+    });
+  }
 
   headerMenu3.addEventListener('click', function(){
-    if (pullDownMenu.getAttribute("style") == "display:block;") {
-      pullDownMenu.removeAttribute("style", "display:block;");
-    } else {
-      pullDownMenu.setAttribute("style", "display:block;");
-    }
+    pullDownMenu.style.display = pullDownMenu.style.display === 'block' ? 'none' : 'block';
   });
 
-  userName.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  userName.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  logOut.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  logOut.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  logIn.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  logIn.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
-
-  userRegistration.addEventListener('mouseover', function(){
-    this.style.textDecoration = 'underline';
-  });
-
-  userRegistration.addEventListener('mouseout', function(){
-    this.style.textDecoration = 'none';
-  });
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', function(){
+      this.style.textDecoration = 'underline';
+    });
+    links[i].addEventListener('mouseout', function(){
+      this.style.textDecoration = 'none';
+    });
+  }
 });
