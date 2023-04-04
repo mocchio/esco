b77e168939eb8f0ba88fafdd9221118d5ca63d00
mocchio <skmt.rome@gmail.com>
2023-04-04 17:31:04 +0900

    js、cssの簡略化

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,27 +1,23 @@
 window.addEventListener('load', function(){
-  const headerMenus = document.querySelectorAll('#header-menu1, #header-menu2, #header-menu3, #header-menu4');
-  const pullDownMenu = document.getElementById('menu3-pull-down');
-  const links = document.querySelectorAll('#user-name, #log-out-btn, #log-in-btn, #user-registration');
+  const headerMenus = document.querySelectorAll(".nav-item");
+  const headerMenu3 = document.getElementById("header-menu3")
+  const pullDownMenu = document.getElementById("menu3-pull-down");
 
-  for (let i = 0; i < headerMenus.length; i++) {
-    headerMenus[i].addEventListener('mouseover', function(){
+  headerMenus.forEach(function(headerMenu) {
+    headerMenu.addEventListener('mouseover', function(){
       this.style.textDecoration = 'underline';
     });
-    headerMenus[i].addEventListener('mouseout', function(){
+
+    headerMenu.addEventListener('mouseout', function(){
       this.style.textDecoration = 'none';
     });
-  }
-
-  headerMenu3.addEventListener('click', function(){
-    pullDownMenu.style.display = pullDownMenu.style.display === 'block' ? 'none' : 'block';
   });
 
-  for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseover', function(){
-      this.style.textDecoration = 'underline';
-    });
-    links[i].addEventListener('mouseout', function(){
-      this.style.textDecoration = 'none';
-    });
-  }
+  headerMenu3.addEventListener('click', function(){
+    if (pullDownMenu.getAttribute("style") == "display:block;") {
+      pullDownMenu.removeAttribute("style", "display:block;");
+    } else {
+      pullDownMenu.setAttribute("style", "display:block;");
+    }
+  });
 });
