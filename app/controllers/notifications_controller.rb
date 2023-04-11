c7dd5a09982ba5453d11430d0ab7345be63b72f5
mocchio <skmt.rome@gmail.com>
2023-04-11 22:24:07 +0900

    notificationsコントローラー記述編集

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,12 +1,12 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_notification, only: [:update, :destroy]
   
   def index
     @notifications = current_user.passive_notifications
   end
 
   def update
-    notification = Notification.find(params[:id])
     if notification.update(checked: true)
       @notifications = current_user.passive_notifications
       redirect_to notifications_path
@@ -14,10 +14,13 @@ class NotificationsController < ApplicationController
   end
 
   def destroy
-    binding.pry
-    notification = Notification.find(params[:id])
     if notification.destroy
       redirect_to notifications_path
     end
+  end
+
+  private
+  def set_notification
+    notification = Notification.find(params[:id])
   end
 end
